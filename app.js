355624cc73d077e89f3a09fb51d670c058fdeba1
rhenzala <97076128+rhenzala@users.noreply.github.com>
2023-09-23 11:57:12 +0800

    Add the Gameboard module which create and render the gameboard
    In the Gameboard, the board is stored in array, and modified starting there
    
    The gameboard is rendered in browser by creating and adding cells to gameBoard div
    
    The state of the board is fetch and stored by the getBoard
    
    Whenever the player mark a cell, the board is updated

---
+++ app.js
@@ -0,0 +1,51 @@
+const Gameboard = (function() {
+    const _gameboard = document.getElementById('gameBoard');
+    let _board = [
+        [null, null, null],
+        [null, null, null],
+        [null, null, null]
+    ];
+
+    _gameboard.addEventListener('click', function(e) {
+        const cell = e.target;
+        const x = cell.dataset.row;
+        const y = cell.dataset.col;
+        GameController.play(x, y);
+    });
+
+    function _createCell(x, y, pick) {
+        const index = x * 3 + parseInt(y);
+        _gameboard.children[index].textContent = pick;
+    }
+    function getBoard() {
+        return _board;
+    }
+
+    function updateBoard(x, y, pick) {
+        if (!_board[x][y]) {
+            _board[x][y] = pick;
+            _createCell(x, y, pick);
+        }
+    }
+
+    function initialize() {
+        for (let i = 0; i < 3; i++) {
+            for (let j = 0; j < 3; j++) {
+                const cell = document.createElement("div");
+                cell.classList.add('cell')
+                cell.dataset.row = i;
+                cell.dataset.col = j;
+                grid.appendChild(cell);
+            }
+        }
+    }
+
+    return {
+        getBoard,
+        updateBoard,
+        initialize
+    };
+})();
+
+
+
